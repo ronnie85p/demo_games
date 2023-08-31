e7004bf2f81315aec2643d65628202c7e2e7b502
Ronnie <romagifted@gmail.com>
2023-08-31 13:12:53 +0700

    add PUT to route api

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,5 @@ Router::post('/api/v1/games', [GamesController::class, 'create']);
 Router::get('/api/v1/games/{id}', [GamesController::class, 'get'])->where(['id' => '[0-9]+']);
 Router::put('/api/v1/games/{id}', [GamesController::class, 'update'])->where(['id' => '[0-9]+']);
 Router::delete('/api/v1/games/{id}', [GamesController::class, 'delete'])->where(['id' => '[0-9]+']);
+
+// fixed
